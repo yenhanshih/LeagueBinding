39ae33015e872effc5666415978b5aaaa606af99
yenhanshih <yenhanshih@gmail.com>
2016-01-30 01:17:37 -0500

    Added test project and updated visual studio version number in solution

---
+++ LeagueBinding.Client.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeagueBinding.Client.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
